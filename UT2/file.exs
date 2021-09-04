437745b647b3c61a8aa187194354c28a785ae6eb
Joaquín Cuitiño <vextil@gmail.com>
2021-09-03 22:16:12 -0300

    stringify working

--- UT2/file.exs
+++ UT2/file.exs
@@ -29,7 +29,9 @@ defmodule JSON do
       {:bool, val} -> inspect(val)
       {:number, val} -> inspect(val)
       {:string, val} -> inspect(val)
-      {:array, val} -> "[" <> Enum.join(Enum.map(val, fn x -> JSON.stringify(x) end), ",") <> "]"
+      {:array, val} -> "[" <> Enum.join(Enum.map(val, fn x -> JSON.stringify(x) end), ", ") <> "]"
+      {:object, val} -> "{" <> Enum.join(Enum.map(val, fn x -> JSON.stringify(x) end), ", ") <> "}"
+      {var, val} -> var <> ": " <> JSON.stringify(val)
       _ -> "otro"
     end
   end
@@ -37,6 +39,7 @@ defmodule JSON do
 end
 
 nums = Enum.map([1,2,3], &({:number, &1}))
+fields = Enum.zip(["x", "y"], [bool: true, bool: false])
 
 IO.puts(JSON.stringify({:null}))
 IO.puts(JSON.stringify({:bool, false}))
@@ -44,3 +47,12 @@ IO.puts(JSON.stringify({:number, -12.34}))
 IO.puts(JSON.stringify({:string, "This string."}))
 IO.puts(JSON.stringify({:array, [{:null}, {:bool, false}]})) # [null, false]
 IO.puts(JSON.stringify(nums))
+IO.puts(JSON.stringify({:object, fields}))
+IO.puts(JSON.stringify({:object, [
+                        {"x", {:number, 1}},
+                        {"y", {:array, []}},
+                        {"z", {:object, [
+                          {"x", {:number, 1}},
+                          {"y", {:array, [{:null}, {:bool, false}]}}
+                        ]}}
+                      ]}))
