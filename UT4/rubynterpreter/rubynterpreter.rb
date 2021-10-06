8aaf13be454b01f19362335c62aefe94a3d6af70
Guillermo Vogel <vogel11@outlook.es>
2021-10-06 10:39:33 -0300

    Carpeta con Ruby interpreter
    - Se sube el intérprete dado por la Cátedra

---
+++ UT4/rubynterpreter/rubynterpreter.rb
@@ -0,0 +1,20 @@
+require_relative 'expressions'
+require_relative 'statements'
+require_relative 'parser'
+
+puts "Welcome to Rubynterpreter."
+puts "Finish your code with an empty line to process it."
+puts ""
+parser = Parser.new
+input = []
+state = {}
+ARGF.each do |line|
+  if (line.strip().empty?)
+    ast = parser.parse_string(input.join('\n'))
+    puts ast
+    # puts ast.unparse
+    input = []
+  else
+    input << line
+  end
+end
