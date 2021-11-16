ab0b752cad308c552a52030457eabf0bd3235275
Joaquín <vextil@gmail.com>
2021-11-16 18:46:02 -0300

    Fix seedrandom

--- UT6/main.js
+++ UT6/main.js
@@ -1,7 +1,7 @@
-import seedrandom from 'seedrandom';
+import { prng_alea } from 'esm-seedrandom';
 import { randomProp, evalProp, truthTable } from './fase0.js';
 
-var random = seedrandom("sddsf");
+var random = prng_alea("sddsf");
 
 var arbol = randomProp(random, ['a', 'b', 'c'], 3, 5);
 console.log(arbol.toString());
