0ea97163e461ad2d14ac0e90b2f7c8f78492baf6
Joaquín <vextil@gmail.com>
2021-11-17 22:55:42 -0300

    Console log

--- UT6/main.js
+++ UT6/main.js
@@ -5,10 +5,10 @@ import { randomTruthTable, fitness, randomSearch } from './fase1.js';
 var rng = prng_alea("sddsf");
 
 var arbol = randomProp(rng, ['a', 'b', 'c'], 3, 5);
-console.log(arbol.toString());
-console.log(evalProp(arbol, { 'a': true, 'b': false, 'c': true }))
-console.log(truthTable(arbol, ['a', 'b', 'c']));
+console.log("Random prop: ", arbol.toString());
+console.log("Eval prop: ", evalProp(arbol, { 'a': true, 'b': false, 'c': true }))
+console.log("Truth table: ", truthTable(arbol, ['a', 'b', 'c']));
 
 var randomTable = randomTruthTable(rng, ['a', 'b', 'c']);
-console.log("Random: ", randomTable);
+console.log("Random truth table: ", randomTable);
 console.log("Fitness: ", fitness(arbol, randomTable));
