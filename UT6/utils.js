3c23630b6175332f7c8dd2ecfffdfc7aa6b7b34b
Joaquín <vextil@gmail.com>
2021-11-19 15:56:00 -0300

    Fase 1 ✅  y headers

--- UT6/utils.js
+++ UT6/utils.js
@@ -1,3 +1,5 @@
+import * as colors from "colors"; 
+
 /**
  * Genera una tabla con todas las permutaciones de true/false para cierta cantidad de elementos.
  * @param {String[]} vars Lista de nombres de variables.
@@ -15,4 +17,23 @@ export function varPermutations(vars) {
         permutations.push(Object.fromEntries(permutation));
     }
     return permutations;
+}
+
+/**
+ * Imprime un header "con estilo" en la consola. Ejemplo:
+ * ===========================================================
+ *                            Title
+ * ===========================================================
+ * @param {Im} title 
+ */
+export function printHeader(title) {
+    const filler = "=";
+    const desiredLength = 60;
+    const sideLength = Math.floor((desiredLength - title.length) / 2);
+    const realLength = title.length + (sideLength * 2);
+    console.log('');
+    console.log(filler.repeat(realLength).black.bgBlue);
+    console.log(title.padStart(sideLength + title.length, ' ').padEnd(realLength, ' ').black.bgBlue);
+    console.log(filler.repeat(realLength).black.bgBlue);
+    console.log('');
 }
