4d141a22ede82a4c4432bd4d95282d175b30d15a
Joaquín <vextil@gmail.com>
2021-11-19 22:39:01 -0300

    fase 2

--- UT6/main.js
+++ UT6/main.js
@@ -1,6 +1,7 @@
 import { prng_alea } from 'esm-seedrandom';
 import { randomProp, evalProp, truthTable } from './fase0.js';
 import { randomTruthTable, fitness, randomSearch } from './fase1.js';
+import { assessPopulation, initialPopulation, selection, evolutionStrategy } from './fase2.js';
 import { printHeader } from './utils.js';
 
 var rng = prng_alea("sddsf");
@@ -10,7 +11,7 @@ var maxHeight = 4;
 var minHeight = 3;
 
 printHeader("randomProp ")
-var arbol = randomProp(rng, vars, maxHeight, minHeight);
+let arbol = randomProp(rng, vars, maxHeight, minHeight);
 console.log(arbol.toString());
 
 printHeader("evalProp");
@@ -20,10 +21,26 @@ printHeader("truthTable");
 console.log(truthTable(arbol, vars));
 
 printHeader("randomTruthTable & fitness");
-var randomTable = randomTruthTable(rng, vars);
+let randomTable = randomTruthTable(rng, vars);
 console.log("Random truth table: ", randomTable);
 console.log("Fitness: ", fitness(arbol, randomTable));
 
-printHeader("randomSearch ");
-var bestRandomProp = randomSearch(rng, randomTable, 5, {vars, maxHeight, minHeight});
+printHeader("randomSearch");
+let bestRandomProp = randomSearch(rng, randomTable, 5, {vars, maxHeight, minHeight});
 console.log("Best prop: ", bestRandomProp, fitness(bestRandomProp, randomTable));
+
+printHeader("initialPopulation");
+let population = initialPopulation(rng, ['a', 'b', 'c'], 10);
+console.log(population);
+
+printHeader("assessPopulation");
+let assessedPopulation = assessPopulation(population, randomTable);
+console.log(assessedPopulation);
+
+printHeader("selection");
+let selectedPopulation = selection(rng, assessedPopulation, 4);
+console.log(selectedPopulation);
+
+printHeader("evolutionStrategy");
+let evolution = evolutionStrategy(rng, randomTable, 100, 100, {vars, maxHeight, minHeight});
+console.log(evolution);
