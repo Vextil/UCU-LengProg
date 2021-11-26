4c02088d2a28dd85190ffb2df383fc4b3293f83f
Joaquín <vextil@gmail.com>
2021-11-26 16:00:42 -0300

    Selección num vars

--- UT6/main.js
+++ UT6/main.js
@@ -2,46 +2,70 @@ import { prng_alea } from 'esm-seedrandom';
 import { randomProp, evalProp, truthTable } from './fase0.js';
 import { randomTruthTable, fitness, randomSearch } from './fase1.js';
 import { assessPopulation, initialPopulation, selection, evolutionStrategy } from './fase2.js';
-import { Conjuncion, Negacion, Variable } from './prop.js';
 import { printHeader } from './utils.js';
+import { createInterface } from 'readline';
 
-var rng = prng_alea("sddsf");
+let rng = prng_alea("sddsf");
 
-var vars = ['a', 'b', 'c'];
-var maxHeight = 4;
-var minHeight = 3;
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-printHeader("randomProp ")
-let arbol = randomProp(rng, vars, maxHeight, minHeight);
-console.log(arbol.toString());
+rl.question("Digite cuántas variables desea utilizar [1,2,3]: ", function (name) {
+    if (name === '1') {
+        runAll(['a'], {'a' : true}, 4, 3);
+    } else if (name === '2') {
+        runAll(['a', 'b'], {'a': true, 'b': false}, 4, 3);
+    } else if (name === '3') {
+        runAll(['a', 'b', 'c'], { 'a': true, 'b': false, 'c': true }, 4, 3);
+    } else {
+        console.log("La cantidad no es válida.")
+    }
+    rl.close();
+});
 
-printHeader("evalProp");
-console.log("Eval prop: ", evalProp(arbol, { 'a': true, 'b': false, 'c': true }))
+rl.on("close", function () {
+    process.exit(0);
+});
 
-printHeader("truthTable");
-console.log(truthTable(arbol, vars));
+function runAll(vars, varValues, maxHeight, minHeight) {
+    printHeader("Variables")
+    console.log(varValues);
 
-printHeader("randomTruthTable & fitness");
-let randomTable = randomTruthTable(rng, vars);
-console.log("Random truth table: ", randomTable);
-console.log("Fitness: ", fitness(arbol, randomTable));
+    printHeader("randomProp")
+    let arbol = randomProp(rng, vars, maxHeight, minHeight);
+    console.log(arbol.toString());
 
-printHeader("randomSearch");
-let bestRandomProp = randomSearch(rng, randomTable, 5, {vars, maxHeight, minHeight});
-console.log("Best prop: ", bestRandomProp, fitness(bestRandomProp, randomTable));
+    printHeader("evalProp");
+    console.log("Eval prop: ", evalProp(arbol, varValues))
 
-printHeader("initialPopulation");
-let population = initialPopulation(rng, ['a', 'b', 'c'], 10);
-console.log(population);
+    printHeader("truthTable");
+    console.log(truthTable(arbol, vars));
 
-printHeader("assessPopulation");
-let assessedPopulation = assessPopulation(population, randomTable);
-console.log(assessedPopulation);
+    printHeader("randomTruthTable & fitness");
+    let randomTable = randomTruthTable(rng, vars);
+    console.log("Random truth table: ", randomTable);
+    console.log("Fitness: ", fitness(arbol, randomTable));
 
-printHeader("selection");
-let selectedPopulation = selection(rng, assessedPopulation, 10);
-console.log(selectedPopulation);
+    printHeader("randomSearch");
+    let bestRandomProp = randomSearch(rng, randomTable, 5, { vars, maxHeight, minHeight });
+    console.log("Best prop: ", bestRandomProp, fitness(bestRandomProp, randomTable));
 
-printHeader("evolutionStrategy");
-let evolution = evolutionStrategy(rng, randomTable, 40, 15, {vars, maxHeight, minHeight});
-console.log(evolution);
+    printHeader("initialPopulation");
+    let population = initialPopulation(rng, vars, 10);
+    console.log(population);
+
+    printHeader("assessPopulation");
+    let assessedPopulation = assessPopulation(population, randomTable);
+    console.log(assessedPopulation);
+
+    printHeader("selection");
+    let selectedPopulation = selection(rng, assessedPopulation, 10);
+    console.log(selectedPopulation);
+
+    printHeader("evolutionStrategy");
+    let evolution = evolutionStrategy(rng, randomTable, 40, 15, { vars, maxHeight, minHeight });
+    console.log(evolution);
+
+}
